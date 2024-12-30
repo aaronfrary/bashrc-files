de0662239e84b0f8018dce1100d806aa74ef9e74
Aaron Frary <aaronfrary@greenriver.org>
2024-12-30 01:06:04 -0500

    Update prompt for zshell

--- fancy_prompt.sh
+++ fancy_prompt.sh
@@ -4,50 +4,6 @@ export GIT_PS1_SHOWSTASHSTATE=1
 export GIT_PS1_SHOWUPSTREAM="auto"
 export GIT_PS1_DESCRIBE_STYLE="branch"
 
-# PROMPT COLORS!!!
-# Reset
-Color_Off='\[\e[0m\]'       # Text Reset
-
-# Regular Colors
-Black='\[\e[0;30m\]'        # Black
-Red='\[\e[0;31m\]'          # Red
-Green='\[\e[0;32m\]'        # Green
-Yellow='\[\e[0;33m\]'       # Yellow
-Blue='\[\e[0;34m\]'         # Blue
-Purple='\[\e[0;35m\]'       # Purple
-Cyan='\[\e[0;36m\]'         # Cyan
-White='\[\e[0;37m\]'        # White
-
-# Background
-On_Black='\[\e[40m\]'       # Black
-On_Red='\[\e[41m\]'         # Red
-On_Green='\[\e[42m\]'       # Green
-On_Yellow='\[\e[43m\]'      # Yellow
-On_Blue='\[\e[44m\]'        # Blue
-On_Purple='\[\e[45m\]'      # Purple
-On_Cyan='\[\e[46m\]'        # Cyan
-On_White='\[\e[47m\]'       # White
-
-# High Intensty
-IBlack='\[\e[0;90m\]'       # Black
-IRed='\[\e[0;91m\]'         # Red
-IGreen='\[\e[0;92m\]'       # Green
-IYellow='\[\e[0;93m\]'      # Yellow
-IBlue='\[\e[0;94m\]'        # Blue
-IPurple='\[\e[0;95m\]'      # Purple
-ICyan='\[\e[0;96m\]'        # Cyan
-IWhite='\[\e[0;97m\]'       # White
-
-# High Intensty backgrounds
-On_IBlack='\[\e[0;100m\]'   # Black
-On_IRed='\[\e[0;101m\]'     # Red
-On_IGreen='\[\e[0;102m\]'   # Green
-On_IYellow='\[\e[0;103m\]'  # Yellow
-On_IBlue='\[\e[0;104m\]'    # Blue
-On_IPurple='\[\e[10;95m\]'  # Purple
-On_ICyan='\[\e[0;106m\]'    # Cyan
-On_IWhite='\[\e[0;107m\]'   # White
-
 # Keep working directory description to a reasonable length.
 abbrev_home_dir() {
   # Recreate '\w' home dir replacement
@@ -91,7 +47,6 @@ prompt() {
   # Check exit code before running any commands.
   local exit_code="$?"
 
-  local sep_color=$Color_Off
   local max5=`tput cols`
   local max1=$(($max5 / 5))
   local max2=$(($max1 + $max1))
@@ -125,26 +80,29 @@ prompt() {
     fi
   fi
 
-  local working_dir=$sep_color'['$Cyan$dir
+  local working_dir='['%F{cyan}$dir%f
 
   local git_info=
   if [[ -n "$git" ]]; then
-    git_info=$br1$sep_color'|'$Yellow$git
+    git_info=$br1'|'%F{yellow}$git%f
   fi
 
-  local status=
+  local last_status=
   if [ $exit_code -ne 0 ]; then
-    status=$Red'#'$White$exit_code
+    last_status=%F{red}'#'%f%F{white}$exit_code%f
   fi
 
-  local prompt_char=$White'\$'
+  local prompt_char=%F{white}'$'%f
   if [ `id -u` = 0 ]; then
-    prompt_char=$Red'#'
+    prompt_char=%F{red}'#'%f
   fi
-  local prompt_end=$sep_color']'$br2$prompt_char' '$Color_Off
+  local prompt_end=']'$br2$prompt_char' '
 
-  PS1=$status$working_dir$git_info$prompt_end
+  PS1=$last_status$working_dir$git_info$prompt_end
 }
 
+# bash
 PROMPT_COMMAND=prompt
 
+# zsh
+precmd () { prompt }
